5c4fa9b175cb799eabd666f60478e9bb7d5a2418
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-10-20 01:39:08 +0200

    :sparkles: Some mock data

--- src/app.js
+++ src/app.js
@@ -10,15 +10,15 @@ class App {
   constructor() {
     const overviewTable = new Table({
       head: [
-        chalk.whiteBright('Balance'),
         chalk.whiteBright('Day'),
         chalk.whiteBright('Progress'),
+        chalk.whiteBright('Balance'),
       ],
     });
     overviewTable.push([
-      `${this.totalBalance.toFixed(2)} BTC`,
       this.day,
       `${this.progress.toFixed(2)}%`,
+      `${this.totalBalance.toFixed(2)} BTC`,
     ]);
 
     const balanceTable = new Table({
@@ -48,15 +48,31 @@ class App {
     );
     console.log('');
 
-    console.log(`${overviewTableStrings[0]}  ${balanceTableStrings[0]}`);
-    console.log(`${overviewTableStrings[1]}  ${balanceTableStrings[1]}`);
-    console.log(`${overviewTableStrings[2]}  ${balanceTableStrings[2]}`);
-    console.log(`${overviewTableStrings[3]}  ${balanceTableStrings[3]}`);
-    console.log(`${overviewTableStrings[4]}  ${balanceTableStrings[4]}`);
+    console.log(`${overviewTableStrings[0]} ${balanceTableStrings[0]}`);
+    console.log(`${overviewTableStrings[1]} ${balanceTableStrings[1]}`);
+    console.log(`${overviewTableStrings[2]} ${balanceTableStrings[2]}`);
+    console.log(`${overviewTableStrings[3]} ${balanceTableStrings[3]}`);
+    console.log(`${overviewTableStrings[4]} ${balanceTableStrings[4]}`);
+
+    const dataTable = new Table({
+      head: [
+        chalk.whiteBright('Day'),
+        chalk.whiteBright('Progress'),
+        chalk.whiteBright('Balance'),
+        chalk.whiteBright('Date'),
+      ],
+      colWidths: [10, 18, 22, 16],
+    });
+
+    dataTable.push([3, '1.03%', '1.031568916 BTC', '2018-10-23']);
+    dataTable.push([2, '0.89%', '0.893464621 BTC', '2018-10-22']);
+    dataTable.push([1, '1.00%', '1.000000000 BTC', '2018-10-21']);
+
+    console.log(dataTable.toString());
   }
 
   get day(): number {
-    return 0;
+    return 3;
   }
 
   get progress(): number {
@@ -72,7 +88,7 @@ class App {
   }
 
   get bitmexBalance(): number {
-    return 0;
+    return 1.031568916;
   }
 
   get coldStorageBalance(): number {
