b3b44b0430d242be082d822286b7106d21b0be9e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-10-20 15:23:28 +0200

    :art: Cleaned up Bitmex class a bit so we can re-use api calls

--- src/bitmex.js
+++ src/bitmex.js
@@ -3,38 +3,46 @@ import axios from 'axios';
 import crypto from 'crypto';
 import { getTime } from 'date-fns';
 
+const apiCall = async (path: string): ?Object => {
+  path = `/api/v1${path}`;
+  const expires = getTime(new Date());
+
+  const signature = crypto
+    .createHmac('sha256', process.env.BITMEX_API_KEY_SECRET)
+    .update(`GET${path}${expires}`)
+    .digest('hex');
+
+  const headers = {
+    'api-expires': expires,
+    'api-key': process.env.BITMEX_API_KEY_ID,
+    'api-signature': signature,
+  };
+
+  try {
+    const response = await axios.get(`https://bitmex.com${path}`, {
+      headers,
+    });
+
+    if (response.status !== 200) {
+      return null;
+    }
+
+    return response.data;
+  } catch (e) {
+    return null;
+  }
+};
+
 class Bitmex {
   static get balance(): number {
-    const path = '/api/v1/user/wallet';
-    const expires = getTime(new Date());
-
-    const signature = crypto
-      .createHmac('sha256', process.env.BITMEX_API_KEY_SECRET)
-      .update(`GET${path}${expires}`)
-      .digest('hex');
-
-    const headers = {
-      'api-expires': expires,
-      'api-key': process.env.BITMEX_API_KEY_ID,
-      'api-signature': signature,
-    };
-
     return (async () => {
-      try {
-        const response = await axios.get(`https://bitmex.com${path}`, {
-          headers,
-        });
+      const data = await apiCall('/user/wallet');
 
-        if (response.status !== 200) {
-          return 0;
-        }
-
-        const { data } = response;
-
-        return data.amount / 100000000;
-      } catch (e) {
+      if (!data) {
         return 0;
       }
+
+      return parseFloat(data.amount / 100000000);
     })();
   }
 }
