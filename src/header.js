a12b82724b5519d7da589db98410c35a5133bdfa
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-10-20 10:53:34 +0200

    :sparkles: Format date

--- src/header.js
+++ src/header.js
@@ -1,13 +1,14 @@
 //@flow
 import fs from 'fs';
 import chalk from 'chalk';
+import { format } from 'date-fns';
 
 const options = { encoding: 'utf-8', flag: 'r' };
 const ascii = fs.readFileSync('resources/ascii.txt', options);
 
 const print = () => {
   const wallet = process.env.BTC_COLD_STORAGE_ADDRESS;
-  const date = process.env.START_DATE;
+  const date = format(process.env.START_DATE, 'MMMM Mo, YYYY');
 
   console.log(chalk.magentaBright(ascii));
   console.log(`Cold storage wallet: ${wallet}, start date: ${date}\n`);
