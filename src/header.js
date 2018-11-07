516f58fbb8c98566a35ffac18065b4e9f7e020c1
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-11-07 13:39:12 +0100

    :lipstick: Changed basic info a bit

--- src/header.js
+++ src/header.js
@@ -7,11 +7,18 @@ const options = { encoding: 'utf-8', flag: 'r' };
 const ascii = fs.readFileSync('resources/ascii.txt', options);
 
 const print = () => {
+  const goal = process.env.BTC_BALANCE_GOAL;
   const wallet = process.env.BTC_WALLET_ADDRESS;
   const date = format(process.env.START_DATE, 'MMMM Do, YYYY');
 
   console.log(chalk.magentaBright(ascii));
-  console.log(`Cold storage wallet: ${wallet}, start date: ${date}\n`);
+  console.log(
+    `${chalk.whiteBright('Goal:')} ${parseInt(goal).toFixed(
+      2,
+    )} BTC ${chalk.whiteBright('Start:')} ${date} ${chalk.whiteBright(
+      'Wallet:',
+    )} ${wallet}\n`,
+  );
 };
 
 export default {
